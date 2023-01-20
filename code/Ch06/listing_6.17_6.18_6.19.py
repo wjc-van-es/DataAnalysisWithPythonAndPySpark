95005d7ea2e0339197ba4ae79970caad4a6b4391
Willem van Es <wjc.van.es@gmail.com>
2023-01-20 23:24:27 +0100

    Chapter 6: part of section 6.4 building your own df schema to filter and enforce the data you want.

---
+++ code/Ch06/listing_6.17_6.18_6.19.py
@@ -0,0 +1,90 @@
+import pyspark.sql.types as T
+import pyspark.sql.functions as F
+import os
+from pyspark.sql import SparkSession
+
+# Setting up a schema definition for shows data
+# where we build up from discrete StructType elements assigned to variables.
+# This should improve readability over a single schema variable with deep nested complex types
+episode_links_schema = T.StructType(
+    [
+        T.StructField(
+            "self", T.StructType([T.StructField("href", T.StringType())])
+        )
+    ]
+)
+
+episode_image_schema = T.StructType(
+    [
+        T.StructField("medium", T.StringType()),
+        T.StructField("original", T.StringType()),
+    ]
+)
+
+episode_schema = T.StructType(
+    [
+        T.StructField("_links", episode_links_schema),
+        T.StructField("airdate", T.DateType()),
+        T.StructField("airstamp", T.TimestampType()),
+        T.StructField("airtime", T.StringType()),
+        T.StructField("id", T.StringType()),
+        T.StructField("image", episode_image_schema),
+        T.StructField("name", T.StringType()),
+        T.StructField("number", T.LongType()),
+        T.StructField("runtime", T.LongType()),
+        T.StructField("season", T.LongType()),
+        T.StructField("summary", T.StringType()),
+        T.StructField("url", T.StringType()),
+    ]
+)
+
+embedded_schema = T.StructType(
+    [
+        T.StructField(
+            "_embedded",
+            T.StructType(
+                [
+                    T.StructField(
+                        "episodes", T.ArrayType(episode_schema)
+                    )
+                ]
+            )
+        )
+    ]
+)
+
+spark = SparkSession.builder.appName("Chapter 6 example").getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+data_dir = "../../data/shows"
+
+# The original json document was a single line and this is the PySpark default rule of:
+# "One JSON document, one line, one (df) record",
+# where multiple JSON docs / df records can fit in one file
+# each on a new line. This is set by the optional parameter multiLine, which has a default of False
+#
+# We already went exploring and ordered the PyCharm IDE to reformat the code, so we get an indented multiline
+# JSON document, which is much easier to read.
+# When we print the schema we now get
+# root
+#  |-- _corrupt_record: string (nullable = true)
+# However, if we set the optional multiline parameter to True it will be able to load the multiline document, which now
+# follow the adapted PySpark rule of
+# "one JSON document, one file, one (df) record"
+df_sil_val = spark.read.json(os.path.join(data_dir, 'shows-silicon-valley.json'), multiLine=True,
+                             schema=embedded_schema, mode='FAILFAST')
+
+# you have one record, where each column is a field on the highest level under the document root
+# hence these columns can have simple values when the corresponding field is a simple scalar type or
+# a hierarchy of nested dataframes when the corresponding field is an object or array (which may in turn contain
+# objects or arrays)
+print(f"total number of records in df_sil_val data frame is  {df_sil_val.count()}")
+
+df_sil_val.printSchema()
+# df_sil_val.show()
+
+for column in ["airdate", "airstamp"]:
+    df_sil_val.select(f"_embedded.episodes.{column}").select(
+        F.explode(column)
+    ).show(5)

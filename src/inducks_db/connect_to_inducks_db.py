5d09c2340718b37721a6699ff0078f02b89cfad2
Willem van Es <wjc.van.es@gmail.com>
2024-05-21 00:28:42 +0200

    willem@willem-Latitude-5590: layout optimized.

--- src/inducks_db/connect_to_inducks_db.py
+++ src/inducks_db/connect_to_inducks_db.py
@@ -5,7 +5,6 @@ from py4j.protocol import Py4JJavaError
 from pyspark.errors import AnalysisException
 from pyspark.sql import SparkSession
 
-
 import project_utils.config_info as ci
 
 ci.print_environment()
@@ -24,10 +23,10 @@ print(f"\npostgres_password: {postgres_password} and inducks_app_password={induc
 jdbc_driver_location = '/home/willem/.m2/repository/org/postgresql/postgresql/42.7.3/postgresql-42.7.3.jar'
 
 spark = (SparkSession.builder
-    .appName("PostgreSQL Connection with PySpark")
-    .config("spark.jars", jdbc_driver_location)
-    .master('local')
-    .getOrCreate())
+         .appName("PostgreSQL Connection with PySpark")
+         .config("spark.jars", jdbc_driver_location)
+         .master('local')
+         .getOrCreate())
 
 url = "jdbc:postgresql://localhost:5432/inducks_db"
 
@@ -37,22 +36,21 @@ properties = {
     "driver": "org.postgresql.Driver"
 }
 
-table_name = 'pub_story_authors_summary' # The summary view of all stories in dutch publications in chronological order
+table_name = 'pub_story_authors_summary'  # The summary view of all stories in Dutch publications in chronological order
 
 try:
     # trying to see all available tables and views of the inducks_schema
     df_table_names = (spark.read.format('jdbc')
-                      .option('url',url) # the same url
-                      .option('driver', properties['driver']) # the same driver
-                      .option('user','postgres') # we need to login as the superuser to see the table info
+                      .option('url', url)  # the same url
+                      .option('driver', properties['driver'])  # the same driver
+                      .option('user', 'postgres')  # we need to login as the superuser to see the table info
                       .option('password', postgres_password)
-                      .option('dbtable','information_schema.tables')
+                      .option('dbtable', 'information_schema.tables')
                       .load()
                       .where(F.col('table_schema') == 'inducks_schema')
                       .select('table_name', 'table_type'))
     df_table_names.printSchema()
     df_table_names.show(20, truncate=False)
-
 
     df = spark.read.jdbc(url, table_name, properties=properties)
     print(f"Reading the {table_name} view from {url}, connected as user {properties['user']} "
@@ -60,7 +58,7 @@ try:
     df.printSchema()
     df.show(10, truncate=False)
 
-    df.createOrReplaceTempView(table_name) # Dit is nog steeds nodig als we sql queries willen gebruiken.
+    df.createOrReplaceTempView(table_name)  # Dit is nog steeds nodig als we sql queries willen gebruiken.
 
     df_willie_wortel = spark.sql(
         f"""
@@ -73,7 +71,7 @@ try:
 
     # print view as a csv
     df.coalesce(1).write.mode("overwrite").csv("./inducks_db_pub_story_authors_summary.csv",
-                                                                            sep='|', header=True, quote=None)
+                                               sep='|', header=True, quote=None)
 
     # stories published in the 'Donald Duck' Weekly in 1977
     df_dd1977 = df.drop('series_code', 'series_title').where(F.col("pub_code").substr(4, 6) == "DD1977")
@@ -84,7 +82,6 @@ except Py4JJavaError as e:
     print(f"problem connecting to {url} as {properties['user']} because of {e!r}")
 except AnalysisException as ae:
     print(f"{ae!r}")
-
 
 if __name__ == "__main__":
     pass
